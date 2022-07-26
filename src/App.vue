87f412dff5538a67cf19588549b2c95ca538e265
Philip Owusu Asare <philip.asare@amalitech.org>
2022-07-26 10:06:58 +0000

    setup and initialization

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <h1>{{ name }} - {{ age }}</h1>
+    <button @click="changeName('Zelda')"> Change Name</button>
+    <button @click="changeAge(34)"> Change Age</button>
+  </div>
 </template>
+
+<script lang="ts">
+    import { defineComponent, reactive, toRefs } from 'vue';
+
+    export default defineComponent({
+      name: 'App',
+      components: {},
+      setup() {
+        const state = reactive({
+          name: 'Link',
+          age: 25 as string | number
+        })
+        state.name = 999  // will not work
+        return { ...toRefs(state)}
+      },
+       
+      methods: {
+        changeName(name: string) {
+          this.name = name
+        },
+        changeAge(age: string | number ) {
+        this.age = age
+      }
+      }, 
+      
+    });
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  // text-align: center;
+  // color: #2c3e50;
 }
 
 nav {
